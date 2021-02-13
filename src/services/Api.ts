2bb148d10dd473e1c1a6208ad47d7e7f07de2a99
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-13 12:38:36 +0100

    Add basic api connection config

---
+++ src/services/Api.ts
@@ -0,0 +1,12 @@
+import axios, { AxiosRequestConfig as LibAxiosRequestConfig } from 'axios';
+
+export interface AxiosRequestConfig extends LibAxiosRequestConfig {}
+
+export const request = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/',
+  responseType: 'json',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});

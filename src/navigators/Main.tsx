36ae9fd4472694aaa87a9c12f421ab069049b4ba
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-13 22:23:30 +0100

    Add custom header config

--- src/navigators/Main.tsx
+++ src/navigators/Main.tsx
@@ -4,13 +4,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 import TabNavigator from './Tab';
 
 import { Details } from '../screens';
+import { HeaderLogo } from '../components';
 
 const Stack = createStackNavigator();
 
 export const Main = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{ headerTitle: (props) => <HeaderLogo {...props} /> }}>
     <Stack.Screen name="Home" component={TabNavigator} />
-    <Stack.Screen name="Details" component={Details} />
+    <Stack.Screen
+      name="Details"
+      component={Details}
+      options={{ headerTitleAlign: 'center' }}
+    />
   </Stack.Navigator>
 );
 

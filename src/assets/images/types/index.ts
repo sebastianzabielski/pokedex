3570128b816026cd2bf95830b6b6219d0284f7a4
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-13 15:03:12 +0100

    Add asset images

---
+++ src/assets/images/types/index.ts
@@ -0,0 +1,24 @@
+import { ImageRequireSource } from 'react-native';
+
+const images: { [key: string]: ImageRequireSource } = {
+  bug: require('./bug.png'),
+  dark: require('./dark.png'),
+  dragon: require('./dark.png'),
+  electric: require('./dark.png'),
+  fairy: require('./dark.png'),
+  fighting: require('./dark.png'),
+  fire: require('./dark.png'),
+  flying: require('./dark.png'),
+  ghost: require('./dark.png'),
+  grass: require('./dark.png'),
+  ground: require('./dark.png'),
+  ice: require('./dark.png'),
+  normal: require('./dark.png'),
+  poison: require('./dark.png'),
+  psychic: require('./dark.png'),
+  rock: require('./dark.png'),
+  steel: require('./dark.png'),
+  water: require('./dark.png'),
+};
+
+export default images;

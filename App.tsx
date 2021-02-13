a86e77a5e833f1f51a38a560343f3c1b30f0d802
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-13 22:22:52 +0100

    Add store provider and navigation container

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {} from 'react-native';
+import { Provider } from 'react-redux';
 
-export default function App() {
+import store from './src/redux/Store';
+import NavigationContainer from './src/navigators';
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer />
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

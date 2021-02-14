5b5c3f170d67aa41aa13ab191a4d787ae08ca90e
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-14 12:23:02 +0100

    Add data reading on init

--- src/screens/Pokemons.tsx
+++ src/screens/Pokemons.tsx
@@ -7,6 +7,8 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import { Pokemon } from '../components';
 import Api from '../services/Api';
 
@@ -16,6 +18,7 @@ import {
   clearStore,
   fetching,
 } from '../redux/PokemonListSlice';
+import { setList, addFavorite } from '../redux/FavoritesSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 export const Footer = () => {
@@ -34,10 +37,17 @@ export const Footer = () => {
 
 export const Pokemons = React.memo(() => {
   const data = useSelector(pokemonList);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const loadStorageData = async () => {
+      const favorites = await AsyncStorage.getItem('favorites');
+      if (favorites) {
+        dispatch(setList(JSON.parse(favorites)));
+      }
+    };
+
+    loadStorageData();
     dispatch(getPokemonList());
   }, []);
 

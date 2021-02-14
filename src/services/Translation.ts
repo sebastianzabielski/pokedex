8c60cfd2f2e22730e9524f7d3c3049f6208daf8c
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-14 21:03:50 +0100

    Add translations

--- src/services/Translation.ts
+++ src/services/Translation.ts
@@ -1,10 +1,13 @@
-const Translation: { [key: string]: string } = {
+const Translation = {
   hp: 'hp',
   attack: 'attack',
   defense: 'defense',
   'special-attack': 'Sp Attack',
   'special-defense': 'Sp Defense',
   speed: 'speed',
+
+  pokemons: 'pokemons',
+  favorites: 'favorites',
 };
 
 export default Translation;

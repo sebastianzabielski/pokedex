cbe8ad69420ac5540e0036f15934161ae5879c12
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-14 20:22:21 +0100

    Create translation

---
+++ src/services/Translation.ts
@@ -0,0 +1,10 @@
+const Translation: { [key: string]: string } = {
+  hp: 'hp',
+  attack: 'attack',
+  defense: 'defense',
+  'special-attack': 'Sp Attack',
+  'special-defense': 'Sp Defense',
+  speed: 'speed',
+};
+
+export default Translation;

d67e2ce859728c80490c033952369478a10a7d85
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2021-02-14 18:49:11 +0100

    Refactor types. Add reducers

--- src/redux/PokemonListSlice.ts
+++ src/redux/PokemonListSlice.ts
@@ -1,33 +1,29 @@
-import { createSlice, Slice, Dispatch, createSelector } from '@reduxjs/toolkit';
+import { createSlice, Dispatch } from '@reduxjs/toolkit';
 import Api from '../services/Api';
-import Store, { RootState } from './Store';
+import { RootState } from './Store';
+import { PokemonBaseModel } from '../models/Pokemon.model';
+import { clearQueue } from './PokemonDetailsSlice';
 
 type State = {
-  pokemonList: {
-    name: string;
-    url: string;
-  }[];
-  next: string;
-  prev: string;
+  pokemonList: PokemonBaseModel[];
   fetching: boolean;
+  searchName: string;
+  error: boolean;
 };
 
 const initialState: State = {
   pokemonList: [],
-  next: 'pokemon/',
-  prev: '',
   fetching: false,
+  searchName: '',
+  error: false,
 };
 
 export const slice = createSlice({
   name: 'pokemonList',
   initialState,
   reducers: {
-    setNext: (state, action) => {
-      state.next = action.payload;
-    },
     setPokemonList: (state, action) => {
-      state.pokemonList = [...state.pokemonList, ...action.payload];
+      state.pokemonList = action.payload;
     },
     clearStore: (state) => {
       state = initialState;
@@ -35,44 +31,93 @@ export const slice = createSlice({
     setFetchingStatus: (state, action) => {
       state.fetching = action.payload;
     },
+    resetList: (state) => {
+      state = initialState;
+    },
+    setSearchName: (state, action) => {
+      state.searchName = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
   },
 });
 
 export const {
-  setNext,
   setPokemonList,
   clearStore,
   setFetchingStatus,
+  resetList,
+  setSearchName,
+  setError,
 } = slice.actions;
 
-export const pokemonList = (state: RootState) => state.list.pokemonList;
+export const pokemonList = (state: RootState) =>
+  state.list.pokemonList.filter((item) =>
+    item.name.toLowerCase().includes(state.list.searchName),
+  );
 
 export const fetching = (state: RootState) => state.list.fetching;
 
+//TODO refactor below
 let getListLocked = false;
-export const getPokemonList = () => async (dispatch: Dispatch) => {
+export const getPokemonList = (url: string = 'pokemon/') => async (
+  dispatch: Dispatch,
+) => {
   if (getListLocked) {
     return;
   }
   getListLocked = true;
   dispatch(setFetchingStatus(true));
-  const data = await fetchPokemonList();
-  dispatch(setNext(data.next));
-  dispatch(setPokemonList(data.data));
-  dispatch(setFetchingStatus(false));
+  clearQueue();
+  dispatch(setPokemonList([]));
+  try {
+    const data: {
+      results: PokemonBaseModel[];
+      next: string;
+    } = await fetchPokemonList(url);
+    dispatch(setPokemonList(data.results));
+    dispatch(setFetchingStatus(false));
+  } catch (err) {
+    dispatch(setError(true));
+  }
+
   getListLocked = false;
 };
 
-const fetchPokemonList = async () => {
+export const getPokemonByTypeList = (url: string) => async (
+  dispatch: Dispatch,
+) => {
+  if (getListLocked) {
+    return;
+  }
+  getListLocked = true;
+  clearQueue();
+  dispatch(setPokemonList([]));
+  dispatch(setFetchingStatus(true));
+  try {
+    const data: {
+      pokemon: PokemonBaseModel[]; //TODO fix type
+    } = await fetchPokemonList(url);
+
+    dispatch(setPokemonList(data.pokemon.map((item) => item.pokemon)));
+    dispatch(setFetchingStatus(false));
+  } catch (err) {
+    dispatch(setError(true));
+  } finally {
+    getListLocked = false;
+  }
+};
+
+const fetchPokemonList = async (url: string) => {
   const { data } = await Api.request({
     method: 'GET',
-    url: Store.getState().list.next,
+    url,
+    params: {
+      limit: 1500,
+    },
   });
-  return {
-    next: data.next,
-    prev: data.previous,
-    data: data.results,
-  };
+  return data;
 };
 
 export default slice.reducer;
